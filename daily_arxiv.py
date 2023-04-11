4502dfe0eebfa007498f6a980711c23c939470ba
CrazyHao <2368184385@qq.com>
2023-04-11 15:40:03 +0900

    submit domain changes

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -349,21 +349,21 @@ def json_to_md(filename,md_filename,
         if show_badge == True:
             # we don't like long string, break it!
             f.write((f"[contributors-shield]: https://img.shields.io/github/"
-                     f"contributors/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[contributors-url]: https://github.com/Vincentqyw/"
-                     f"cv-arxiv-daily/graphs/contributors\n"))
-            f.write((f"[forks-shield]: https://img.shields.io/github/forks/Vincentqyw/"
-                     f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[forks-url]: https://github.com/Vincentqyw/"
-                     f"cv-arxiv-daily/network/members\n"))
-            f.write((f"[stars-shield]: https://img.shields.io/github/stars/Vincentqyw/"
-                     f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[stars-url]: https://github.com/Vincentqyw/"
-                     f"cv-arxiv-daily/stargazers\n"))
-            f.write((f"[issues-shield]: https://img.shields.io/github/issues/Vincentqyw/"
-                     f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-            f.write((f"[issues-url]: https://github.com/Vincentqyw/"
-                     f"cv-arxiv-daily/issues\n\n"))
+                     f"contributors/CrazyHao/cv-arxiv-deilei.svg?style=for-the-badge\n"))
+            f.write((f"[contributors-url]: https://github.com/CrazyHao/"
+                     f"cv-arxiv-deilei/graphs/contributors\n"))
+            f.write((f"[forks-shield]: https://img.shields.io/github/forks/CrazyHao/"
+                     f"cv-arxiv-deilei.svg?style=for-the-badge\n"))
+            f.write((f"[forks-url]: https://github.com/CrazyHao/"
+                     f"cv-arxiv-deilei/network/members\n"))
+            f.write((f"[stars-shield]: https://img.shields.io/github/stars/CrazyHao/"
+                     f"cv-arxiv-deilei.svg?style=for-the-badge\n"))
+            f.write((f"[stars-url]: https://github.com/CrazyHao/"
+                     f"cv-arxiv-deilei/stargazers\n"))
+            f.write((f"[issues-shield]: https://img.shields.io/github/issues/CrazyHao/"
+                     f"cv-arxiv-deilei.svg?style=for-the-badge\n"))
+            f.write((f"[issues-url]: https://github.com/CrazyHao/"
+                     f"cv-arxiv-deilei/issues\n\n"))
                 
     logging.info(f"{task} finished")        
 
